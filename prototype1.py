de10c80d444e4ba6457fb258b86a2a0102296529
Mateus <39527468+mateuszanotto@users.noreply.github.com>
2019-07-02 15:40:44 -0300

    Add files via upload

--- prototype1.py
+++ prototype1.py
@@ -3,20 +3,17 @@ import openbabel
 from openbabel import pybel
 
 
-#pega o arquivo smile.txt e transforma em um module de strings com as linhas
-#aaa
-#bbb
-#smile = ['aaa', 'bbb']
-with open('smiles.smi', 'r') as file:
+#pega o arquivo smile.txt e transforma em um module de strings
+#o arquivo .txt não pode ter linha em branco no final
+with open('smiles.txt', 'r') as file:
 	smile = file.read().splitlines();
 	#para pegar as linhas com aaa\n usar file.readlines()
 
 #pega os valores do module e le como smile
 n = 0
 while (n < len(smile)):
-	xyz = pybel.readstring("smi", smile[n])
-	output = pybel.Outputfile("xyz", 'a_{}.xyz'.format(n), overwrite=False)
+	xyz = pybel.readstring('smi', smile[n])
+	output = pybel.Outputfile('xyz', 'a_{}.xyz'.format(n), overwrite=True)
 	xyz.make3D(forcefield='mmff94', steps=50)
 	output.write(xyz)
 	n+=1;
-
