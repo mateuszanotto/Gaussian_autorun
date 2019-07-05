2ea54f0a875929dbff2a782151457b264fd897c5
Mateus <39527468+mateuszanotto@users.noreply.github.com>
2019-07-05 11:12:39 -0300

    Add files via upload

--- prototype3.py
+++ prototype3.py
@@ -9,14 +9,16 @@
 # importar openbabel
 import openbabel
 from openbabel import pybel
+from pathlib import Path
 import subprocess
+import re
 
-# transforma cada linha do smiles.txt num item de uma lista. O arquivo smiles.txt NÃO pode ter linha em branco no final
+# transforma cada linha do smiles.txt num item de uma lista. O arquivo smiles.txt NAO pode ter linha em branco no final
 with open('smiles.txt', 'r') as file:
 	Smiles = file.read().splitlines() # para pegar as linhas com 'aaa\n' usar file.readlines()
 
-with open('input/run_all.sh', 'w+') as file:
-	file.write('#!/bin/bash\n\n')
+# Define o caminho ate o script
+path = Path('~/github/db_singletoxygen-master')
 
 # escreve n arquivos onde n eh a quantidade de smiles no arquivo smiles.txt
 n = 0
@@ -35,20 +37,45 @@ while (n < len(Smiles)):
 
 	# adiciona os inputs do gaussian no .com
 	with open('input/input_{}.com'.format(n), 'w') as file:
-		lines[0] = '%nprocs=8 \n%mem=16GB \n%chk=molecule_{}.chk \n#opt freq b3lyp/6-31g(d,p) \n\ninput {}\n\n'.format(n, n) # processadores, memoria, chk, input e nome
+		lines[0] = '%nprocs=1 \n%mem=2GB \n#hf/6-31g(d,p) \n\nmolecule_{} {}\n\n'.format(path, n, n, smi) # processadores, memoria, chk, input e nome(molecule_No Smile)  \n%chk={}/log/molecule_{}.chk
 		lines[1] = '0 1\n' # multiplicidade e carga
 		file.writelines(lines) #sobrescreve as linhas 0 e 1 com as infos acima
 
     # adiciona algo ao fim do arquivo .com
 	with open('input/input_{}.com'.format(n), 'a') as file:
- 		file.write('\n') # linha em branco (necessário pro input do gaussian)
+ 		file.write('\n') # linha em branco (necessario pro input do gaussian)
 
-	with open('input/run_all.sh', 'a') as file:
-		file.write('g09 < molecule_{}.com > molecule_{}.log &&\n'.format(n, n))
-		if (n < len(Smiles)):
-			lastline=[n]
-			with open('input/run_all.sh', 'r') as file:
-				for word in file.readlines():
-					lastline.append(word.replace('&&', ''));
+	# cria um job para cada molecula
+	with open('input/job_{}.sh'.format(n), 'w') as file:
+		file.write('#!/bin/bash\n\ncd {}/input\ng09 < input_{}.com >  {}/log/molecule_{}.log'.format(path, n, path, n))
+
+	subprocess.run('chmod a+x {}/input/job_{}.sh'.format(path, n), shell=True)
 
 	n += 1;
+
+#############
+i = 0
+while (i < len(Smiles)):
+	word = 'Normal Termination'
+	normalterm = 0
+	if Path('{}/log/molecule_{}.log'.format(path, i)).is_file() is True: # verifica se o .log existe ################## dando erro, entrando se o arquivo NAO EXISTE
+
+		# checa se o .log tem 0, 1 ou 2 normal terminations
+		with open('{}/log/molecule_{}.log'.format(path, i), 'r') as file:
+			normterm = str(file.readlines())
+			ntcounter = len(list(re.finditer('Normal termination of Gaussian 09', normterm)))
+			if ntcounter == 0: # 0 normal termination = erro opt
+				print('Molecula {} opt error termination'.format(ntcounter));
+			if ntcounter == 1: # 1 normal termination = erro freq
+				print('Molecula {} freq error termination'.format(ntcounter));
+			elif ntcounter == 2:
+				None # 2 normal termination = normal
+
+	if Path('{}/log/molecule_{}.log'.format(path, i)).is_file() is False: ################## dando erro, entrando se o arquivo EXISTE
+		print('Rodando Molecula {}'.format(i)) # aviso no terminal
+		subprocess.call('{}/input/job_{}.sh'.format(str(path), i), shell=True) # rodar job se o .log n existir
+
+	else:
+		None
+
+	i += 1;
