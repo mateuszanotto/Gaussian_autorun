4701fa8cc77797f088bbb01571749dfb1f0b6852
Mateus Z <mateus@ABCSim>
2019-10-17 16:14:44 -0300

    go

--- Gaussian_autorun.py
+++ Gaussian_autorun.py
@@ -1,23 +1,33 @@
 import openbabel
+import sys
+print('Python version:{}'.format(sys.version))
 from openbabel import pybel
 from pathlib import Path
 import subprocess
 import re
+import scipy as sp
+print('SciPy version:{}'.format(sp.__version__))
+import numpy as np
+print('numpy version:{}'.format(np.__version__))
+import pandas as pd
+print('pandas version:{}'.format(pd.__version__))
 
 class Gaussian_autorun():
     '''This class turn smiles into inputs, runs Gaussian and create an database
     '''
     print('''
-    ######################################
-    #              mestrado              #
-    #            outubro 2019            #
-    #       smile -> input gaussian      #
-    #               patch 0.8            #
-    #           mateus m z toledo        #
-    ######################################''')
+    ######################################                         _
+    #              mestrado              #        _        ,-.    / )
+    #                                    #       ( `.     // /-._/ /
+    #            outubro 2019            #        `\ \   /(_/ / / /
+    #       smile -> input gaussian      #          ; `-`  (_/ / /
+    #                                    #          |       (_/ /
+    #               patch 0.8            #          \          /
+    #           mateus m z toledo        #           )       /`
+    ######################################           /      /`\n\n ''')
 
-    def __init__(self, path, a):
-        self.a = a
+    def __init__(self, path, typeCalc):
+        self.typeCalc = typeCalc
         self.normal = []
         self.error = []
         if (str(path) != None):
@@ -37,43 +47,51 @@ class Gaussian_autorun():
         for n in range(len(self.smiles)):
             smi = pybel.readstring('smi', self.smiles[n])
             smi.make3D(forcefield='mmff94', steps=50)
-            output = pybel.Outputfile('xyz', 'input/{self}_input_{n}.com'.format(self=self.a, n=n), overwrite=True)
+            output = pybel.Outputfile('xyz', 'input/{self}_input_{n}.com'.format(self=self.typeCalc, n=n), overwrite=True)
             output.write(smi)
 
-            with open('input/{self}_input_{n}.com'.format(self=self.a, n=n), 'r') as file:
+            with open('input/{self}_input_{n}.com'.format(self=self.typeCalc, n=n), 'r') as file:
                 lines = file.readlines()
-            with open('input/{self}_input_{n}.com'.format(self=self.a, n=n), 'w') as file:
+            with open('input/{self}_input_{n}.com'.format(self=self.typeCalc, n=n), 'w') as file:
                 lines[0] = '''%nprocs={calc[0]}
 %mem={calc[1]}GB
 %chk={path}/chk/{self}_molecule_{n}.chk
 {calc[2]}
 \nmolecule_{n} {smi}
-\n0 1'''.format(self=self.a, calc=self.calc, path=self.path , n=n, smi=self.smiles[n])
+\n0 1'''.format(self=self.typeCalc, calc=self.calc, path=self.path , n=n, smi=self.smiles[n])
                 lines[-1] += '\n'
                 file.writelines(lines)
                 file.close()
-            with open('input/{self}_job_{n}.sh'.format(self=self.a, n=n), 'w') as file:
+            with open('input/{self}_job_{n}.sh'.format(self=self.typeCalc, n=n), 'w') as file:
                 file.write('''#!/bin/bash
 cd {path}/input
-g09 < {self}_input_{n}.com > {path}/log/{self}_molecule_{n}.log'''.format(self=self.a, path=self.path, n=n))
-            subprocess.run('chmod a+x {path}/input/{self}_job_{n}.sh'.format(self=self.a, path=self.path, n=n), shell=True) # cria input.com e job.sh
+g09 < {self}_input_{n}.com > {path}/log/{self}_molecule_{n}.log'''.format(self=self.typeCalc, path=self.path, n=n))
+            subprocess.run('chmod a+x {path}/input/{self}_job_{n}.sh'.format(self=self.typeCalc, path=self.path, n=n), shell=True) # cria input.com e job.sh
 
     def Run(self):
         for n in range(len(self.smiles)):
             try:
-                open('{path}/log/{self}_molecule_{n}.log'.format(path=self.path, self=self.a,  n=n), 'r')
+                open('{path}/log/{self}_molecule_{n}.log'.format(path=self.path, self=self.typeCalc,  n=n), 'r')
             except FileNotFoundError:
-                print('\nRunning {self} calculation - Molecule {n}'.format(self=self.a, n=n)) # aviso no terminal
-                subprocess.call('{path}/input/{self}_job_{n}.sh'.format(path=self.path, self=self.a,  n=n), shell=True) # rodar job se o .log n existir
+                print('\nRunning {self} calculation - Molecule {n}'.format(self=self.typeCalc, n=n)) # aviso no terminal
+                subprocess.call('{path}/input/{self}_job_{n}.sh'.format(path=self.path, self=self.typeCalc,  n=n), shell=True) # rodar job se o .log n existir
 
     def Error(self):
         for n in range(len(self.smiles)):
-            with open('{path}/log/{self}_molecule_{n}.log'.format(path=self.path, self=self.a,  n=n), 'r') as file:
+            with open('{path}/log/{self}_molecule_{n}.log'.format(path=self.path, self=self.typeCalc,  n=n), 'r') as file:
                 normterm = str(file.readlines())
                 self.normal.append(len(list(re.finditer('Normal termination of Gaussian 09', normterm))))
-                self.error.append(len(list(re.finditer('Error termination', normterm))))
-                if self.error==0:
+                if self.normal==0:
                     print('\nMolecule {n} error termination'.format(n=n))
-        if sum(self.error)==0:
-            print('\n- No {self} error termination -\n'.format(self=self.a))
-            print('List of {self} Normal terminations: \n'.format(self=self.a)+ str(self.normal))
+        if sum(self.normal)==0:
+            print('\n- No {self} error termination -\n'.format(self=self.typeCalc))
+#            print('List of {self} Normal terminations: \n'.format(self=self.typeCalc)+ str(self.normal))
+        elif sum(self.normal)!=len(self.normal):
+            print('\n- ERROR TERMINATION ON {self} (ﾉ｀□´)ﾉ⌒┻━┻ - \n'.format(self=self.typeCalc))
+            print('List of {self} Error terminations:'.format(self=self.typeCalc))
+            for n in range(len(self.smiles)):
+                if self.normal[n]==0:
+                    print('Molecule {n} ERROR termination'.format(n=n))
+            print('┬─┬ノ(ಠ_ಠノ)')
+        elif sum(self.normal)==len(self.normal):
+            print('{self} Normal terminated ʕᵔᴥᵔʔ\n '.format(self=self.typeCalc))
