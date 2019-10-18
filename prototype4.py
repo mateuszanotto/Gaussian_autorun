c0fee22450bb3b182a71d31710411d38e6c115b7
Mateus Z <mateus@ABCSim>
2019-10-18 07:45:01 -0300

    troquei os nomes de umas variaveis e coloquei pra checar se as pastas existem

--- prototype4.py
+++ prototype4.py
@@ -9,18 +9,17 @@
 
 import Gaussian_autorun as ga
 from pathlib import Path
+import os
 
 ## define o caminho até a pasta e o cálculo
-path = '/home/mateus/github/db_singletoxygen'
+path = os.getcwd()
 typeCalc = 'opt'
 calc = ['8', #processadores
         '16', #memoria em GB
         '#p opt b3lyp/6-31g(d)'] # calculo
 
 
-opt = ga.Gaussian_autorun(path, typeCalc)
-opt.calc = calc
-opt.path = path
+opt = ga.Gaussian_autorun(path, name, calc)
 opt.Inputs()
 opt.Run()
 opt.Error()
@@ -28,16 +27,14 @@ opt.Error()
 #-----------------------------------------#
 
 ## define o caminho até a pasta e o cálculo
-path = '/home/mateus/github/db_singletoxygen'
-typeCalc = 'freq'
+path = os.getcwd()
+name = 'freq' #nome no começo do arquivo
 calc = ['8', #processadores
         '16', #memoria em GB
         '#p freq b3lyp/6-31g(d)'] # calculo
 
 
-opt = ga.Gaussian_autorun(path, typeCalc)
-opt.calc = calc
-opt.path = path
+opt = ga.Gaussian_autorun(path, name, calc)
 opt.Inputs()
 opt.Run()
 opt.Error()
